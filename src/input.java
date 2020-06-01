05e5ea8b2ebb42d877b38be918834561b61da9fe
ses <ruzaev.artem21@gmail.com>
2020-06-01 19:29:20 -0400

    yo

--- src/input.java
+++ src/input.java
@@ -9,9 +9,15 @@ public class input {
         while (!userInput.matches("[a-zA-Z]{1}")) {
             System.out.println("Please enter a single letter only.");
             userInput = scan.nextLine();
-
-            
         }
+        char[] confirm = new char[4];
+        confirm[0] = 'Y';
+        confirm[1] = 'N';
+        confirm[2] = 'y';
+        confirm[3] = 'n';
 
+        System.out.println("Your input is " + userInput + " confirm? (Y/N)");
+        char userConfirmation = scan.next().charAt(0);
+        if (userConfirmation != )
     }
 }
